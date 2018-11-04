a9ea611ffa8691b7742122bbe25853963706f983
sticinska <sticinska@gmail.com>
2018-11-04 15:44:12 +0200

    asdsada

--- gramatas.php
+++ gramatas.php
@@ -11,10 +11,32 @@ require_once("templates/header.php");
 <h1 id="heading">Seit ir visas kategorijas un apakskategorijas</h1>
 <?php
 
-$sql="SELECT DISTINCT Kategorija FROM kategorija;";
-echo "<!-- $sql -->";
-$sql_res = mysqli_query($d,$sql) or die("<h1>".mysqli_error()."</h1>");
-tabula($sql_res);
+$query="SELECT * FROM kategorija WHERE Virskategorija = Null;";
+$results = mysql_query($query);
+
+$resultset = array();
+while ($row = mysql_fetch_array($results)) {
+  $resultset[] = $row;
+}
+
+
+
+foreach ($resultset as $result){
+    $categoryID = $result['ID'];
+    $categoryName = $result['Kategorija'];
+    
+    echo "<a href='#'>$categoryName</a>";
+    
+    $query="SELECT * FROM kategorija WHERE Virskategorija = $categoryID;";
+    $resultscat = mysql_query();
+    while($row = mysql_fetch_array($resultscat)){
+        $childCategoryName = $row['Kategorija'];
+        echo "<a href='#'>$childCategoryName</a>";
+    }
+
+}
+
+
 
 ?>
 <hr>
