e57a2d331c6d3be2dc92d6ad673cfd029178e37d
sticinska <sticinska@gmail.com>
2018-11-04 13:11:54 +0200

    iok;

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost','dbteh','DBteh-2018','world') or die('Nevaru pievienoties datubāzei');
+$conn = mysqli_connect('10.0.15.134','dbteh','DBteh-2018','library') or die('Nevaru pievienoties datubāzei');
 $chs=mysqli_set_charset($conn, "utf8");
 
 ?>
