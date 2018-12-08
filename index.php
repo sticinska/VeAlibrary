03fb887a3903e40b0af9c8c70891168dc6bbdd44
sticinska <sticinska@gmail.com>
2018-12-08 15:45:00 +0200

    asd

--- index.php
+++ index.php
@@ -11,11 +11,10 @@ require_once("templates/header.php");
 <?php
 
 
-$sql="SELECT Nosaukums, CONCAT (autors.Vards, ' ' , autors.Uzvards) as Autors , autors.ID as AID, gramata.ID as GID FROM gramata FULL JOIN
-   gramatasAutori ON ID = gramatasAutori.GramataID LEFT JOIN autors ON autors.ID = gramatasAutori.AutorsID;";
+$sql="SELECT Nosaukums, CONCAT(autors.Vards, ' ' , autors.Uzvards) as Autors  FROM gramata FULL JOIN
+gramatasAutori ON ID = gramatasAutori.GramataID LEFT JOIN autors ON autors.ID = gramatasAutori.AutorsID;";
    $sql_res = mysqli_query($conn,$sql) or die("<h1>no</h1>");
    while ($row = mysqli_fetch_assoc($sql_res)) {
-     echo 'please';
      echo '<a href="autors.php?id=' .$row["AID"]. '">' .$row["Autors"]. '</a href="gramata.php?id='.$row["GID"].'><a>'.$row["Nosaukums"].'</a><br>';
    }
 
