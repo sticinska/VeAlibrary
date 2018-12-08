8e5019cc46c44b5886b3f10a50bd6c0a2b911767
sticinska <sticinska@gmail.com>
2018-12-08 16:04:36 +0200

    a

--- gramata.php
+++ gramata.php
@@ -3,10 +3,9 @@
 require_once("templates/header.php");
 if ( isset( $_GET[ "id" ] ) )
    $bookID = $_GET["id"];
-
+   echo $bookID;
    $sql="SELECT Nosaukums, CONCAT(autors.Vards, ' ' , autors.Uzvards) as Autors , autors.ID as ID FROM gramata 
-   FULL JOIN
-      gramatasAutori ON GramataaID = gramatasAutori.GramataID LEFT JOIN autors ON autors.ID = gramatasAutori.AutorsID WHERE GramataaID = ".$bookID.";";
+   FULL JOIN gramatasAutori ON GramataaID = gramatasAutori.GramataID LEFT JOIN autors ON autors.ID = gramatasAutori.AutorsID WHERE GramataaID = ".$bookID.";";
    $sql_res = mysql_query($conn,$sql) or die("<h1>".mysqli_error()."</h1>");
    while ($row = mysqli_fetch_assoc($sql_res)) {
      echo '<div>' .$row["Autors"]. '</div>';
