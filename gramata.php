7de201626bf1faf8a7affbc5d18c444a126a1a92
sticinska <sticinska@gmail.com>
2018-12-11 21:23:11 +0200

    ads

--- gramata.php
+++ gramata.php
@@ -4,14 +4,18 @@ require_once("templates/header.php");
 if ( isset( $_GET[ "id" ] ) )
    $bookID = $_GET["id"];
 
-   $sql="SELECT Nosaukums, eksemplars.IrPieejama, Count(*) as Skaits FROM gramata LEFT JOIN eksemplars ON eksemplars.Gra
-   mata = GramataaID WHERE GramataaID=? GROUP BY IrPieejama;";
+   $sql="SELECT Nosaukums, eksemplars.IrPieejama, Count(*) as Skaits FROM gramata 
+         LEFT JOIN eksemplars 
+         ON eksemplars.Gramata = GramataaID 
+         WHERE GramataaID=? 
+         GROUP BY IrPieejama;";
+
    $stmt = mysqli_prepare($conn, $sql);
    mysqli_stmt_bind_param($stmt, "s", $bookID);
    $stmt->execute();
    $sql_res = $stmt->get_result();
    
-   echo '<div><h3>' .$row["Skaits"]. '</h3><br><h3>"'.$row["Nosaukums"].'"</h3></div>';
+   echo '<div><h3>'.$row["Nosaukums"].'"</h3></div>';
    
 
   
