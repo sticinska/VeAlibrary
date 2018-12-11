f2dd9037187f64f7e8be2ae87ad57fcd1524a249
sticinska <sticinska@gmail.com>
2018-12-11 19:25:27 +0200

    sdad

--- index.php
+++ index.php
@@ -13,9 +13,13 @@ require_once("templates/header.php");
 
 $sql="SELECT Nosaukums, CONCAT(autors.Vards, ' ' , autors.Uzvards) as Autors, GramataaID as GID, autors.ID as AID FROM gramata FULL JOIN gramatasAutori ON GramataaID = gramatasAutori.GramataID LEFT JOIN autors ON autors.ID = gramatasAutori.AutorsID;";
    $sql_res = mysqli_query($conn,$sql) or die("<h1>no</h1>");
+   echo "<center><table class=\"schedule\">";
+   $first = true;
+    echo "</table></center>";
    while ($row = mysqli_fetch_assoc($sql_res)) {
-     echo '<div><a href="autors.php?id=' .$row["AID"]. '">' .$row["Autors"]. '</a> ';
-     echo '<a href="gramata.php?id='.$row["GID"].'">'.$row["Nosaukums"].'</a></div><br>';
+    
+     echo '<tr><a href="autors.php?id=' .$row["AID"]. '">' .$row["Autors"]. '</a> ';
+     echo '<a href="gramata.php?id='.$row["GID"].'">'.$row["Nosaukums"].'</a></tr><br>';
    }
 
 include_once("templates/footer.php");
