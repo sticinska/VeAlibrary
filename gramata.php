47659f5e6f84feffac6bcf4aa026e05c38e6645d
sticinska <sticinska@gmail.com>
2018-12-11 21:35:10 +0200

    autori for book

--- gramata.php
+++ gramata.php
@@ -3,7 +3,7 @@
 require_once("templates/header.php");
 if ( isset( $_GET[ "id" ] ) )
    $bookID = $_GET["id"];
-   echo $bookID;
+
    $sql="SELECT Nosaukums, eksemplars.IrPieejama, Count(*) as Skaits FROM gramata 
          LEFT JOIN eksemplars 
          ON eksemplars.Gramata = GramataaID 
@@ -16,6 +16,17 @@ if ( isset( $_GET[ "id" ] ) )
    $sql_res = $stmt->get_result();
    $row = mysqli_fetch_array($sql_res);
    echo '<div><h3>"'.$row["Nosaukums"].'"</h3></div>';
+
+   $sql="SELECT CONCAT(autors.Vards, ' ' , autors.Uzvards) as Autors FROM gramatasAutori LEFT JOIN autors ON autors.ID = gramatasAutori.AutorsID WHERE GramataID=?;"
+    $stmt = mysqli_prepare($conn, $sql);
+   mysqli_stmt_bind_param($stmt, "s", $bookID);
+   $stmt->execute();
+   $sql_res = $stmt->get_result();
+   while($row = mysqli_fetch_array($sql_res)){
+         echo '<div><p>'.$row["Autors"].'</p></div>';
+   }
+   
+
 
    
 
