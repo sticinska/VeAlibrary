4142ade5fe19502d5e2cf1597a33a68d0deba800
sticinska <sticinska@gmail.com>
2018-12-12 22:48:14 +0200

    dasd

---
+++ pievienoAutoru.php
@@ -0,0 +1,29 @@
+<?php
+
+try{
+    $pdo = new PDO("mysql:host=localhost;dbname=library", "dbteh", "DBteh-2018");
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
+
+$sql = "INSERT INTO autors (Vards, Uzvards, IzcelsmesValsts) 
+                    VALUES ('".$_POST["Vards"]."',".$_POST['Uzvards']."',".$_POST['Valsts'].")";
+        echo '1';
+    $stmt = $pdo->query($sql);
+    echo '2';
+    $stmt->execute();
+    echo '3';
+            
+    
+    header("Location:pievieno.php"); 
+    
+    echo "Records inserted successfully.";
+    
+     
+    // Close connection
+    unset($pdo);
+
+?>
