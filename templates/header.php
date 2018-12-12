8b2de8f2434789aebf5fd66acc4fea664238a0fa
sticinska <sticinska@gmail.com>
2018-12-12 23:36:35 +0200

    asdsad

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@ require_once("config.php");
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
+    <meta charset="UTF-8">
     <title>VeA Bibliotēka</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="imp/main.css" />
