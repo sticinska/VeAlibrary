447b1b66dccc54c3b98816eb96cd3fcd8b38da62
sticinska <sticinska@gmail.com>
2018-12-13 22:15:17 +0200

    asdsad

--- gramata.php
+++ gramata.php
@@ -17,12 +17,12 @@ if ( isset( $_GET[ "id" ] ) )
    $row = mysqli_fetch_array($sql_res);
    echo '<div><h3>"'.$row["Nosaukums"].'"</h3></div>';
 
-   $sql="SELECT CONCAT(autors.Vards, ' ' , autors.Uzvards) as Autors, AutorsID as AID FROM gramatasAutori LEFT JOIN autors ON autors.ID = gramatasAutori.AutorsID WHERE GramataID=?;"
-   $stmt = mysqli_prepare($conn, $sql);
-   mysqli_stmt_bind_param($stmt, "s", $bookID);
-   $stmt->execute();
-   $sql_res = $stmt->get_result();
-   while($row = mysqli_fetch_array($sql_res)){
+   $sql = $pdos->prepare("SELECT CONCAT(autors.Vards, ' ' , autors.Uzvards) as Autors, AutorsID as AID 
+                          FROM gramatasAutori 
+                          LEFT JOIN autors ON autors.ID = gramatasAutori.AutorsID 
+                          WHERE GramataID='$bookID';");
+   $sql->execute();
+   while($row = $sql->fetch()){
          echo '<div><p>'.$row["Autors"].'</p></div>';
    }
 
