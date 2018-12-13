7a3534aecc4be681fc952a794a86bea9e8b590d7
sticinska <sticinska@gmail.com>
2018-12-13 18:47:12 +0200

    asdsad

--- pievienoEksemplaru.php
+++ pievienoEksemplaru.php
@@ -2,7 +2,7 @@
 require_once("templates/header.php");
 
 $stmt = $conn->prepare("INSERT INTO eksemplars (Gramata, BiblSvitrkods) VALUES (?, ?, GETDATE(), 1)");
-$stmt->bind_param('issi', $g, $s);
+$stmt->bind_param('is', $g, $s);
         
 $g = $_POST["Gramata"];
 $s = $_POST["bibsvitras"];
