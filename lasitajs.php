fd6736c70c1d7a0a691c4d475a07ed3cc03bb7e4
sticinska <sticinska@gmail.com>
2018-12-17 00:09:46 +0200

    dasdasd

--- lasitajs.php
+++ lasitajs.php
@@ -3,7 +3,9 @@ require_once("templates/header.php");
 
 if ( isset( $_GET[ "id" ] )){
     $lasID = $_GET["id"];
-
+    echo "<h1>";
+    echo $lasID;
+    echo "</h1>";
     $sql="SELECT gramata.Nosaukums AS Gramata, AizdosanasDatums AS Izdota, 
     IF(IrNodota,"Nodota","Izdota lasisanai") AS Statuss 
     FROM izdevums 
@@ -16,7 +18,7 @@ if ( isset( $_GET[ "id" ] )){
     $stmt = mysqli_prepare($conn, $sql);
     $stmt->execute();
     $IZDEVUMI = $stmt->get_result();
-
+    echo "<h1>get result</h1>";
     while ($row = mysqli_fetch_assoc($IZDEVUMI)) {
         echo '<p>"'.$row["Gramata"].'" | '.$row["Izdota"]. ' | '.$row["Statuss"].'</p>';
     }
