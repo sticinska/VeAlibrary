0e9b67cd3f82d5152ac511829a0c0a0d88e7d810
sticinska <sticinska@gmail.com>
2018-12-16 22:19:29 +0200

    dasdasd

--- gramata.php
+++ gramata.php
@@ -39,13 +39,15 @@ if ( isset( $_GET[ "id" ] ) )
       
    }
 
-   $sql = $pdos->prepare("SELECT CONCAT(autors.Vards, ' ' , autors.Uzvards) as Autors, AutorsID as AID 
-                          FROM gramatasAutori 
-                          LEFT JOIN autors ON autors.ID = gramatasAutori.AutorsID 
-                          WHERE GramataID='$bookID';");
-   $sql->execute();
-   while($row = $sql->fetch()){
-         echo '<div><p>'.$row["Autors"].'</p></div>';
+   $sql="CALL gramatasAutori('$bookID')";
+   
+   $stmt = mysqli_prepare($conn, $sql);
+   mysqli_stmt_bind_param($stmt, "s", $bookID);
+   $stmt->execute();
+   $sql_res = $stmt->get_result();
+
+   while($row =  $row = mysqli_fetch_array($sql_res)){
+      echo '<div><p>'.$row["Autors"].'</p></div>';
    }
 
   
