942567580d118fff305651d9cd60b05026e88336
sticinska <sticinska@gmail.com>
2018-12-16 23:59:54 +0200

    dasdasd

--- lasitaji.php
+++ lasitaji.php
@@ -8,7 +8,7 @@ $stmt->execute();
 $LIETOTAJI = $stmt->get_result();
 echo '<h2>Lasitaji </h2>';
 
-while ($row = mysqli_fetch_assoc($Gr)) {
+while ($row = mysqli_fetch_assoc($LIETOTAJI)) {
     echo '<a href="lasitajs.php?id='.$row["ID"].'"><h5>"'.$row["Lietotajvards"].'"</h5></a>';
 }
 
