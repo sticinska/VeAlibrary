3fcc2b4f18c3f0f04142f1cf59f9702c2143f790
sticinska <sticinska@gmail.com>
2018-12-16 18:30:06 +0200

    dasdasd

--- index.php
+++ index.php
@@ -12,11 +12,17 @@ require_once("templates/header.php");
    $stmt = mysqli_prepare($conn, $sql);
    mysqli_stmt_bind_param($stmt, "s", $skaits);
    $stmt->execute();
+   $Gramatas = array();
    $res = $stmt->get_result();
    while ($row = mysqli_fetch_assoc($res)) {
-    echo '<a href="gramata.php?id='.$row["GID"].'">"'.$row["Nosaukums"].'"</h5></a>'; 
+    $gr = array($row["GID"], $row["Nosaukums"]);    
+    array_push($Gramatas,$gr);
    }
 
+   foreach($Gramatas as &$value) {
+    $gramata = $value[1];
+    $gramatasID = $value[0];
+     
 /*
      $sql1="SELECT * FROM autors;";
      $stmt1 = mysqli_prepare($conn, $sql1);
@@ -26,8 +32,9 @@ require_once("templates/header.php");
        // echo '<a href="autors.php?id=' .$autorsRow["AID"]. '"><h5 class="mt-0 mb-1">'.$autorsRow["Autors"].' </a>';
      //}
      
-     
-    
+     echo '<a href="gramata.php?id='.$gramatasID.'">"'.$gramata.'"</h5></a>';
+     echo 'default graamatas apraksts</div></li></div>';
+    }
 
 ?>
     
