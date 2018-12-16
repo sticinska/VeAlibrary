12576f6a4f45321dc5ebda4eda2858c54b94f551
sticinska <sticinska@gmail.com>
2018-12-16 23:06:38 +0200

    dasdasd

--- config.php
+++ config.php
@@ -17,11 +17,11 @@ function tabula($sql_res) {
         echo "<tr>";
             foreach ($row as $v) {
                 echo "<td>$v</td>";
-                echo "<td> <form action='dzestAutoru.php' autocomplete='no' method='post'>";
-                echo "<input type='hidden' value='" + $row["ID"] + "'>";
-                echo "<input type='submit' value='delete'></form></td>";
+                
             }
-           
+            echo "<td> <form action='dzestAutoru.php' autocomplete='no' method='post'>";
+            echo "<input type='hidden' value='" + $row["ID"] + "'>";
+            echo "<input type='submit' value='delete'></form></td>";
             echo "</tr>".PHP_EOL;
     }
     echo "</table></center>";
