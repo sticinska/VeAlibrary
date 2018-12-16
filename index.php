e264fbc2f81f17cbe81087f69a65d6b321799c41
sticinska <sticinska@gmail.com>
2018-12-16 20:13:53 +0200

    dasdasd

--- index.php
+++ index.php
@@ -17,29 +17,18 @@ require_once("templates/header.php");
     $gr = array($row["GID"], $row["Nosaukums"]);    
     array_push($Gramatas,$gr);
    }
-   $sql1="SELECT * FROM autors;";
-    
-   $stmt1 = mysqli_prepare($conn, $sql1);
-    if(!$stmt1->execute()){
-        $stmt1->error;
-    }
+   
 
    foreach($Gramatas as &$value) {
     $gramata = $value[1];
     $gramatasID = $value[0];
      
     
-     $sql1="SELECT * FROM autors;";
-    
-     $stmt1 = mysqli_prepare($conn, $sql1);
-      if(!$stmt1->execute()){
-          $stmt1->error;
-      }
      //while($autorsRow = mysqli_fetch_assoc($res)){
        // echo '<a href="autors.php?id=' .$autorsRow["AID"]. '"><h5 class="mt-0 mb-1">'.$autorsRow["Autors"].' </a>';
      //}
      
-     echo '<a href="gramata.php?id='.$gramatasID.'">"'.$gramata.'"</h5></a>';
+     echo '<a href="gramata.php?id='.$gramatasID.'"><h5>"'.$gramata.'"</h5></a>';
     }
 
 ?>
