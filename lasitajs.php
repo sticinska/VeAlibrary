2a8bbcd962f3ddf8dc958855cb7cd1a74d869e79
sticinska <sticinska@gmail.com>
2018-12-17 02:09:48 +0200

    dasdasd

--- lasitajs.php
+++ lasitajs.php
@@ -8,7 +8,7 @@ if ( isset( $_GET[ "id" ] )){
     echo "</h1>";
     
     $sql="SELECT gramata.Nosaukums AS Gramata, AizdosanasDatums AS Izdota, 
-    IF(IrNodota,"Nodota","Izdota lasisanai") AS Statuss 
+    IF(IrNodota,'Nodota','Izdota lasisanai') AS Statuss 
     FROM izdevums 
     LEFT JOIN eksemplars 
     ON GramatasEksemplars = eksemplars.BiblSvitrkods  
@@ -17,12 +17,12 @@ if ( isset( $_GET[ "id" ] )){
     WHERE lasitajs=".$lasID." ORDER BY Statuss asc";
 
     $stmt = mysqli_prepare($conn, $sql);
-    /*$stmt->execute();
+    $stmt->execute();
     $IZDEVUMI = $stmt->get_result();
     echo "<h1>get result</h1>";
     while ($row = mysqli_fetch_assoc($IZDEVUMI)) {
         echo '<p>"'.$row["Gramata"].'" | '.$row["Izdota"]. ' | '.$row["Statuss"].'</p>';
-    }*/
+    }
 
 }
    
