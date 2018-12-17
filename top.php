730cbd13d117773aa7483068cf46a5b9f9231902
sticinska <sticinska@gmail.com>
2018-12-17 03:06:33 +0200

    dasdasd

--- top.php
+++ top.php
@@ -6,7 +6,43 @@ $sql="SELECT mostPopular() as ID;";
 $stmt = mysqli_prepare($conn, $sql);
 $stmt->execute();
 $row = mysqli_fetch_assoc($stmt->get_result());
-echo $row["ID"];
+
+$bookID = $row["ID"];
+
+   $sql="CALL gramataFullInfo('$bookID')";
+   
+   $stmt = mysqli_prepare($conn, $sql);
+   mysqli_stmt_bind_param($stmt, "s", $bookID);
+   $stmt->execute();
+   $sql_res = $stmt->get_result();
+   $row = mysqli_fetch_array($sql_res);
+   echo '<div><h3>"'.$row["Gramata"].'"</h3></div>';
+   echo '<div><h5>Apjoms: '.$row["Apjoms"].'</h5></div>';
+   echo '<div><h5>ISBN: '.$row["ISBN"].'</h5></div>';
+   echo '<div><h5>Izdevuma gads: '.$row["IzdevumaGads"].'</h5></div>';
+   $rowcount = ($sql_res->num_rows);
+   $statuss = $row["Statuss"];
+   $eksSkaits = $row["EksSkaits"];
+   if($rowcount==2){
+      $row = mysqli_fetch_array($sql_res);
+      if($statuss <=> "Pieejama"){
+            echo '<div><h5>'.$statuss.': '.$eksSkaits.'</h5></div>';
+            echo '<div><h5>'.$row["Statuss"].': '.$row["EksSkaits"].'</h5></div>';
+      }
+   }else{
+      if($eksSkaits == 0){
+            echo '<div>Nav pieejami šīs grāmatas eksemplāri.</div>';
+      }else{
+            if($statuss <=> "Pieejama"){
+                  echo '<div><h5>'.$statuss.': '.$eksSkaits.'</h5></div>';
+                  echo '<div><h5>Izdota lasīšanai: 0</h5></div>';
+            }else{
+                  echo '<div><h5>'.$statuss.': '.$eksSkaits.'</h5></div>';
+                  echo '<div><h5>Pieejama: 0</h5></div>';
+            }
+      }
+      
+   }
 
 
 
